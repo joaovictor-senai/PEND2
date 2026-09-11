4e8a9bbda6f2fbc1ddd14367528354eb380595bb
joaovictor-senai <joao.gamero@aluno.senai.br>
2026-09-11 11:31:40 -0300

    11-09-2026

--- javascript/API/camera/script.js
+++ javascript/API/camera/script.js
@@ -1,10 +1,51 @@
+
+const video = document.querySelector("#camera");
+
+const canvas = document.querySelector("#canvas");
+
+const botao = document.querySelector("#tirarfoto");
+
+const foto = document.querySelector("#foto");
+
 navigator.mediaDevices.getUserMedia({
-        video: true, audio: true
-    })
+
+    video: true
+
+})
 .then(function(stream) {
-    const video = document.querySelector("#camera");
+
     video.srcObject = stream;
+
 })
 .catch(function(error) {
-    console.log("Nao foi possivel acessar a camera." , error);
+
+    console.log("Nao foi possivel acessar a camera.", error);
+
 });
+
+botao.addEventListener("click", function() {
+
+    canvas.width = video.clientWidth;
+
+    canvas.height = video.clientHeight;
+
+    const contexto = canvas.getContext("2d");
+
+    contexto.drawImage(
+
+        video,
+
+        0,
+
+        0,
+
+        canvas.width,
+
+        canvas.height
+
+    );
+
+    foto.src = canvas.toDataURL("image/png");
+
+});
+
